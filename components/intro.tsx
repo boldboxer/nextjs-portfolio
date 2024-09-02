38f4848344834ff0355ce4e1d7ea6bb17d0ff737
Nezzer Mose <ebesteinpr@gmail.com>
2024-09-02 19:17:35 +0300

    linter error

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,9 +5,9 @@ export default function Intro() {
   return (
     <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
       <div className='mt-2 flex-1 md:mt-0'>
-        <h1 className='title no-underline'>Hey, I'm Nyamunga.</h1>
+        <h1 className='title no-underline'>Hey, I&#39;m Nyamunga.</h1>
         <p className='mt-3 font-light text-muted-foreground'>
-          I'm a software engineer based in Nairobi, Kenya. I'm
+        I&#39;m a software engineer based in Nairobi, Kenya. I&#39;m
           passionate about learning new technologies and sharing knowledge with
           others.
         </p>
