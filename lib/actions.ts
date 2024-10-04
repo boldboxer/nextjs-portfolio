d83958d931d408fe708c8a03f72604e9f6ae5b4f
Nezzer Mose <ebesteinpr@gmail.com>
2024-10-04 23:53:01 +0300

    testing subscribers component

--- lib/actions.ts
+++ lib/actions.ts
@@ -44,21 +44,22 @@ export async function subscribe(data: NewsletterFormInputs) {
     return { error: result.error.format() }
   }
 
-  // try {
-  //   const { email } = result.data
-  //   const { data, error } = await resend.contacts.create({
-  //     email: email,
-  //     audienceId: process.env.RESEND_AUDIENCE_ID as string
-  //   })
+  try {
+    const { email } = result.data
+    const { data, error } = await resend.contacts.create({
+      email: email,
+      audienceId: process.env.RESEND_AUDIENCE_ID as string
+    })
 
-  //   if (!data || error) {
-  //     throw new Error('Failed to subscribe')
-  //   }
+    if (!data || error) {
+      throw new Error('Failed to subscribe')
+    }
+    
     // TODO: Send a welcome email
 
     return { success: true }
-  // } catch (error) {
-  //   console.error("Error creating contact:", error);
-  //   return { error }
-  // }
+  } catch (error) {
+    console.error("Error creating contact:", error);
+    return { error }
+  }
 }
